ede6abbe211d0d1ba067c527ba4f650995df830b
Basil Nikityuk <basil.nikityuk@gmail.com>
2017-05-20 10:51:26 +0300

    Adds drag'n'drop

---
+++ MacpViewer/ImageItem.h
@@ -0,0 +1,13 @@
+//
+//  ImageItem.h
+//  MacpViewer
+//
+//  Created by Basil Nikityuk on 5/20/17.
+//  Copyright (c) 2017 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ImageItem : NSObject
+
+@end
---
+++ MacpViewer/ImageItem.m
@@ -0,0 +1,13 @@
+//
+//  ImageItem.m
+//  MacpViewer
+//
+//  Created by Basil Nikityuk on 5/20/17.
+//  Copyright (c) 2017 __MyCompanyName__. All rights reserved.
+//
+
+#import "ImageItem.h"
+
+@implementation ImageItem
+
+@end
